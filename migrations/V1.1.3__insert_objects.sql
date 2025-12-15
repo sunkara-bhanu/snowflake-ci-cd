8607f6ce7b62817444fca5eda7140f27a3d0fc2d
sunkara-bhanu <bhanuprakash.sunkara@gmail.com>
2025-12-15 10:21:44 +0530

    inserting values to the table

---
+++ migrations/V1.1.3__insert_objects.sql
@@ -0,0 +1,6 @@
+USE DATABASE DEMO_DB;
+USE SCHEMA DEMO;
+
+-- Insert into the table
+insert into HELLO_WORLD (FIRST_NAME,LAST_NAME,AGE) values 
+                        ("Bhanu Prakash","SUNKARA",38);
