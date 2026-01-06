f838f5e7ff85927a53773e9f58e22e52e3c54cc4
sunkara-bhanu <bhanuprakash.sunkara@gmail.com>
2026-01-06 12:12:32 +0530

    Inserting values to table

--- migrations/V1.1.3__insert_objects.sql
+++ migrations/V1.1.3__insert_objects.sql
@@ -2,5 +2,5 @@ USE DATABASE DEMO_DB;
 USE SCHEMA DEMO;
 
 -- Insert into the table
-insert into HELLO_WORLD (FIRST_NAME,LAST_NAME,AGE) values 
+insert into  USER_DETAILS (FIRST_NAME,LAST_NAME,CITY,AGE) values 
                         ('John','Doe','New York',40);
